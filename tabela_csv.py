b0fe6886a9bef2f45fa10871b3c80f63a07fc15b
Pino Pavlin <pino.pavlin03@gmail.com>
2024-10-30 00:21:37 +0100

    Popravljene spremenljivke in zakomentiran print.

--- tabela_csv.py
+++ tabela_csv.py
@@ -2,21 +2,27 @@ import csv
 from pridobi_podatke_podstrani import pridobi_kazni_za_drzavo, drzave
 
 # Ime datoteke, kamor bomo shranili podatke
-csv_file = "kazni_evropske_drzave.csv"
+csv_datoteka = "kazni_evropske_drzave.csv"
 
 # Funkcija za zapisovanje podatkov v CSV
 def shrani_v_csv(podatki):
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+
+    with open(csv_datoteka, mode='w', newline='', encoding='utf-8') as file:
+
         writer = csv.writer(file)
+
         # Zapis glave tabele
         writer.writerow(['Država', 'Valuta', 'Kazni za alkohol', 'Kazni za prehitro vožnjo', 'Kazni za varnostni pas', 'Kazni za mobilni telefon'])
-        print(podatki)
+
+        # print(podatki)
+
         # Zapis podatkov za vsako državo
-        for val in podatki:
-            writer.writerow([val['drzava'], val['valuta'], val['alkohol'], val['voznja'], val['pas'], val['telefon']])
+        for vrednost in podatki:
+            writer.writerow([vrednost['drzava'], vrednost['valuta'], vrednost['alkohol'], vrednost['voznja'], vrednost['pas'], vrednost['telefon']])
 
 # Glavna funkcija za pridobitev podatkov in shranjevanje v CSV
 def zberi_podatke_in_shrani():
+
     vsi_podatki = []
 
     for drzava in drzave:
@@ -26,7 +32,8 @@ def zberi_podatke_in_shrani():
             vsi_podatki.append(kazni)
 
     shrani_v_csv(vsi_podatki)
-    print(f"Podatki uspešno shranjeni v datoteko {csv_file}.")
+
+    # print(f"Podatki uspešno shranjeni v datoteko {csv_datoteka}.")
 
 # Zaženi program
 zberi_podatke_in_shrani()
