ae21de4eac850f182ecd5ebcf590e395adb000b7
Pino Pavlin <pino.pavlin03@gmail.com>
2024-10-30 00:23:23 +0100

    Popravljene spremenljivke, komentarji.

--- pretvori_v_EUR.py
+++ pretvori_v_EUR.py
@@ -3,12 +3,12 @@ import re
 from pridobi_podatke_podstrani import pridobi_kazni_za_drzavo, drzave
 
 # Ime datoteke, kamor bomo shranili podatke
-csv_file = "kazni_evropske_drzave_eur_sortirano.csv"
+csv_datoteka = "kazni_evropske_drzave_eur_sortirano.csv"
 
 # Pretvornik valut
-conversion_rates = {
+pretvorbe = {
     'EUR': 1,       # Evro je referenčna valuta
-    'USD': 0.925,   # Primer tečaja za ameriški dolar (1 USD = 0.92 EUR)
+    'USD': 0.925,   # Primer tečaja za ameriški dolar (1 USD = 0.925 EUR)
     'GBP': 1.198,
     'ALL': 0.01,
     'BYN': 0.281,
@@ -41,31 +41,31 @@ def pretvori_v_eur(cena, valuta):
         if match:
             # print(match.group(1).replace(".",""))
             # Vrni prvo številko kot minimum in jo pretvori v EUR
-            return round(float(match.group(1).replace(".","").replace(",",".")) * conversion_rates.get(valuta), 2)
+            return round(float(match.group(1).replace(".","").replace(",",".")) * pretvorbe.get(valuta), 2)
         # Preveri, če je cena samo ena številka
         match = re.search(r'\d+.\d*', cena)
         if match:
             # print(match.group(0).replace(".",""))
-            return round(float(match.group(0).replace(".","").replace(",",".")) * conversion_rates.get(valuta), 2)
+            return round(float(match.group(0).replace(".","").replace(",",".")) * pretvorbe.get(valuta), 2)
     except Exception as e:
         return "Napaka pri obdelavi"
     return 0
 
 # Funkcija za zapisovanje podatkov v CSV z minimalnimi kaznimi, pretvorjenimi v EUR
 def shrani_v_csv_eur(podatki):
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+    with open(csv_datoteka, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         # Zapis glave tabele
         writer.writerow(['Država', 'Valuta', 'Min kazni za alkohol (EUR)', 'Min kazni za prehitro vožnjo (EUR)', 'Min kazni za varnostni pas (EUR)', 'Min kazni za mobilni telefon (EUR)'])
         # Zapis podatkov za vsako državo
-        for val in podatki:
+        for vrednost in podatki:
             writer.writerow([
-                val['drzava'],
-                val['valuta'],
-                pretvori_v_eur(val['alkohol'], val['valuta']),
-                pretvori_v_eur(val['voznja'], val['valuta']),
-                pretvori_v_eur(val['pas'], val['valuta']),
-                pretvori_v_eur(val['telefon'], val['valuta'])
+                vrednost['drzava'],
+                vrednost['valuta'],
+                pretvori_v_eur(vrednost['alkohol'], vrednost['valuta']),
+                pretvori_v_eur(vrednost['voznja'], vrednost['valuta']),
+                pretvori_v_eur(vrednost['pas'], vrednost['valuta']),
+                pretvori_v_eur(vrednost['telefon'], vrednost['valuta'])
             ])
 
 # Glavna funkcija za pridobitev podatkov in shranjevanje v CSV
@@ -78,11 +78,11 @@ def zberi_podatke_in_shrani_eur():
         if kazni:
             vsi_podatki.append(kazni)
 
-    # Pretvori cene v EUR in jih uredi po ceni kazni za varnostni pas
+    # Pretvori cene v EUR in jih uredi po ceni kazni za varnostni pas padajoče
     vsi_podatki.sort(key=lambda x: pretvori_v_eur(x['pas'], x['valuta']), reverse = True)
 
     shrani_v_csv_eur(vsi_podatki)
-    print(f"Podatki uspešno shranjeni v datoteko {csv_file}.")
+    # print(f"Podatki uspešno shranjeni v datoteko {csv_datoteka}.")
 
 # Zaženi program
 zberi_podatke_in_shrani_eur()
